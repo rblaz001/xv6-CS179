af9670c5611508032be3141a9af50cfae8d95790
Raudel Blazquez <rblaz001@ucr.edu>
2020-03-15 12:43:02 -0700

    Pair Programming: Updated kernelthread tests with comments and mutual exlusion for print statements.

--- kernelthreadtests.c
+++ kernelthreadtests.c
@@ -1,13 +1,20 @@
 #include "types.h"
 #include "user.h"
 
-struct arguments;
-struct extreme_arguments;
+struct frisB_args;
+struct extreme_frisB_args;
+struct sem_args;
 
-void criticalFuction(int* sem);
-void frisbee(struct arguments * arg);
-void extreme_frisbee(struct extreme_arguments * arg);
-int numberOfThrows;
+void criticalFuction(struct sem_args* sem);
+void frisbee(struct frisB_args * arg);
+void extreme_frisbee(struct extreme_frisB_args * arg);
+
+// argument structure for criticalFunctions
+struct sem_args{
+  int sem;           // Offset used to reference semaphore
+  int mutex;         // Offset used to reference semaphore
+  int threadNum;     // Used to identify thread 
+};
 
 // This is a test for kernel level thread synchronization using semaphores.
 // Generate 4 user threads that are set to enter a critical function.
@@ -20,11 +27,16 @@ void semaphoreTest()
   int stdout = 1;
   printf(stdout, "Starting semaphoreTest\n");
 
-  int sem = sem_initialize(2); // Initialize semaphore for 2 threads to enter critical section
+  int sem;
+  int mutex;
 
-  if(sem == -1)
+  sem = sem_initialize(2);    // Initialize semaphore for 2 threads to enter critical section
+  mutex = sem_initialize(1);  // Initialize semaphore for mutual exclusion
+
+  if(sem == -1 || mutex == -1)
   {
-    printf(stdout, "PANIC\n");
+    printf(stdout, "PANIC: Failed to initialize semaphore\n");
+    return;
   }
   else
   {
@@ -34,56 +46,76 @@ void semaphoreTest()
   // Generate 4 kernel level threads with same entry point
   for(int i = 0; i < 4; i++)
   {
-    KT_Create((void*)&criticalFuction, (void*)&sem);
+    struct sem_args* args = (struct sem_args*) malloc(sizeof(struct sem_args));
+    args->threadNum = i;
+    args->mutex = mutex;
+    args->sem = sem;
+    KT_Create((void*)&criticalFuction, (void*)args);
   }
 
   // Waiting for all threads to finish
   for(int i = 0; i < 4; i++)
   {
+    // Wrap print statemetns with mutex lock aquisition
+    sem_wait(mutex);
     printf(stdout, "Waiting to Join thread %d\n", i);
+    sem_signal(mutex);
     KT_Join();
   }
-  sem_free(sem);    // Freeing lock
+  sem_free(sem);
+  sem_free(mutex);
 
   printf(stdout, "semaphoreTest ok\n");
 }
 
 // Critical section where stdout is used to periodically display status of thread
-void criticalFuction(int* sem)
+void criticalFuction(struct sem_args* args)
 {
   int stdout = 1;
+  sem_wait(args->mutex);
   printf(stdout, "Calling Sem Wait");
-
-  if(sem_wait(*sem) == -1)
-  {
-    printf(stdout, "panic\n");
-    exit(); 
-  } 
+  sem_signal(args->mutex);
+ 
+  sem_wait(args->sem);
   for(int i = 1; i <= 1000000; i++)
   {
     if(i % 10000 == 0)
+    {
+      sem_wait(args->mutex);
       printf(stdout, "%d\n", i/100);
+      sem_signal(args->mutex);
+    }
 
     if(i == 1000000)
+    {
+      sem_wait(args->mutex);
       printf(stdout, "Finished\n");
+      sem_signal(args->mutex);
+    }
   }
-  sem_signal(*sem);
+  sem_signal(args->sem);
 
+  free(args);
   exit();
 }
 
-struct arguments {
-  int player;
-  int sem;
-  int numPlayers;
-  int numThrows;
-  int * thrower;
-  int * counter;
+// argument structure for frisbee function
+struct frisB_args {
+  int player;           // Used to identify thread as particular player
+  int sem;              // Offset used to reference semaphore
+  int numPlayers;       // Total players playing the game
+  int numThrows;        // Total number of throws needed before game ends
+  int * thrower;        // Pointer to value indicating who's turn it is to throw frisbee
+  int * counter;        // Number frisbee throws
 };
 
 
+// Objective of game is to have frisbee thrown numThrows amount of times
+// The frisbee needs to be thrown in round robin
+// Paramaters: int numPlayers, int numThrows
+// numplayers: number of players that are playing frisbee
+// numThrows: number of throws before the game ends
 void frisbeeGame(int numPlayers, int numThrows){
-  numberOfThrows = numThrows;
   int thrower = 0;
   int counter = 1;
   int sem = sem_initialize(1);
@@ -94,7 +126,7 @@ void frisbeeGame(int numPlayers, int numThrows){
   }
 
   for (int i = 0; i < numPlayers; i++){
-    struct arguments * arg = (struct arguments *) malloc(sizeof(struct arguments));
+    struct frisB_args * arg = (struct frisB_args *) malloc(sizeof(struct frisB_args));
     arg->player = i;
     arg->sem = sem;
     arg->numPlayers = numPlayers;
@@ -108,11 +140,13 @@ void frisbeeGame(int numPlayers, int numThrows){
   {
     KT_Join();
   }
-
-  exit();
 }
 
-void frisbee(struct arguments * arg){
+// This is the function that contains the critical section
+// In the critical section we check if it is our turn to throw the frisbee
+// If it is, we declare our throw and throw the frisbee otherwise we just exit critical section
+// Threads continue to repeat this process until the maximum number of throws is reached
+void frisbee(struct frisB_args * arg){
   while (1) {
 
     sem_wait(arg->sem);
@@ -136,26 +170,33 @@ void frisbee(struct arguments * arg){
     sem_signal(arg->sem);
   }
 
+  free(arg);
   exit();
 }
 
 struct frisbees {
-    int thrower;
-    int counter;
+    int thrower;    // Identifies who's turn it is to throw frisbee
+    int counter;    // Number of frisbee throws
   };
 
-struct extreme_arguments {
-  int player;
-  int sem;
-  int sem_mutex;
-  int numPlayers;
-  int numThrows;
-  int numFrisbees;
-  struct frisbees * frisbees;
+struct extreme_frisB_args {
+  int player;                     // Used to identify thread as particular player
+  int sem;                        // Offset used to reference semaphore
+  int sem_mutex;                  // Offset used to reference semaphore
+  int numPlayers;                 // Total players playing the game
+  int numThrows;                  // Number of throws needed before game ends. For each frisbee
+  int numFrisbees;                // Number of frisbees in play 
+  struct frisbees * frisbees;     // Pointer to structs holding status of frisbees
 };
 
+
+// Objective of game is to have each frisbee thrown numThrows amount of times
+// The frisbees needs to be thrown in round robin
+// Paramaters: int numPlayers, int numThrows, int numFrisbees
+// numplayers: number of players that are playing frisbee
+// numThrows: number of throws before the game ends
+// numFrisbees: number of frisbees used in game
 void extremeFrisbeeGame(int numPlayers, int numThrows, int numFrisbees){
-  numberOfThrows = numThrows;
   int sem = sem_initialize(numFrisbees);
   int sem_mutex = sem_initialize(1);
 
@@ -172,7 +213,7 @@ void extremeFrisbeeGame(int numPlayers, int numThrows, int numFrisbees){
   }
 
   for (int i = 0; i < numPlayers; i++){
-    struct extreme_arguments * arg = (struct extreme_arguments *) malloc(sizeof(struct extreme_arguments));
+    struct extreme_frisB_args * arg = (struct extreme_frisB_args *) malloc(sizeof(struct extreme_frisB_args));
     arg->player = i;
     arg->sem = sem;
     arg->sem_mutex = sem_mutex;
@@ -188,10 +229,10 @@ void extremeFrisbeeGame(int numPlayers, int numThrows, int numFrisbees){
     KT_Join();
   }
 
-  exit();
+  free(frisbees);
 }
 
-int isEndOfGame(struct extreme_arguments * arg){
+int isEndOfGame(struct extreme_frisB_args * arg){
   for (int i =0; i < arg->numFrisbees; i++){
     if(arg->frisbees[i].counter != arg->numThrows + 1) return 0;
   }
@@ -199,7 +240,11 @@ int isEndOfGame(struct extreme_arguments * arg){
   return 1;
 }
 
-void extreme_frisbee(struct extreme_arguments * arg){
+// This is the function that contains the critical section
+// In the critical section we check if it is our turn to throw each frisbee
+// If it is, we declare our throw and throw the frisbee otherwise we just exit critical section
+// Threads continue to repeat this process until the maximum number of throws is reached
+void extreme_frisbee(struct extreme_frisB_args * arg){
   while (1) {
 
     sem_wait(arg->sem);
@@ -232,5 +277,6 @@ void extreme_frisbee(struct extreme_arguments * arg){
     sem_signal(arg->sem);
   }
 
+  free(arg);
   exit();
 }
